7e5867ee7526ba03803fcc5ef1ca36f33e999ce2
nam pham <Administrator@PC211>
2020-06-12 15:55:23 +0700

    page 29

--- BigSchool/Controllers/CoursesController.cs
+++ BigSchool/Controllers/CoursesController.cs
@@ -31,8 +31,15 @@ namespace BigSchool.Controllers
 
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(CourseViewModel viewModel)
         {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+
             var course = new Course
             {
                 LecturerId = User.Identity.GetUserId(),
